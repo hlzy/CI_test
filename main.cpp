e1acaf388c44bfca4d06ba3e897ae4d5f4c75028
何炼 <lian.he@bitmain.com>
2019-05-30 20:48:07 +0800

    update

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+typedef void  runtime;
+int main(){
+//  vector<int> a(10,1);
+//  vector<int> b(3,2);
+//  b = a ;
+  int a[2][5] = {{1,2,3,4,5},{2,3,4,5,6}};  
+  for(int i =0 ;i<2;i++)
+      for(int j =0 ;j<5;j++)
+      cout << a[i][j]<< endl;
+//  cout<<a.size()<<endl;
+//  cout<<&a[0] <<endl;
+//  cout<<a[1]<<endl;
+//  cout<<a.back()<<endl;
+//  cout<<a.size()<<endl;
+//  cout<<a[2]<<endl;
+//  runtime * test;
+}

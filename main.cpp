2cf282ea0b6d1b4387e447500d65f949806e5ea2
hlzy <327842846@qq.com>
2019-05-30 21:58:03 +0800

    1
    1

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(){
 //  vector<int> a(10,1);
 //  vector<int> b(3,2);
 //  b = a ;
-  int a[2][5] = {{1,2,3,4,5},{2,3,4,5,6}};  
+  int a[2][5] = {{1,2,3,4,5},{2,3,4,5,6}}
   for(int i =0 ;i<2;i++)
       for(int j =0 ;j<5;j++)
       cout << a[i][j]<< endl;
